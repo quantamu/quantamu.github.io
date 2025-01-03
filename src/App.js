10fced2a312770e63f005e9c77bdd927684a9649
Samuel jose Molero gonzalez <samuel@Mac-5162.lan>
2025-01-02 21:05:15 -0600

    Update to Home

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-
+import logo from './assets/logo2.png'
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <header className="Website-header">
 
-        <div class = "header-content">
+        <div className = "header-content">
           <a href = "App.js">
-            <img src ={`${process.env.PUBLIC_URL}/assets/logo.png`} className="header-logo" alt="logo"> </img>
+            <img src ={logo} className="header-logo" alt="logo"/> 
           </a>
-            
         </div>
-        
+        <button className = "join-us-button">Join Us</button>
+      </header>
       <center>
         <nav>
             <a href = "#About Us">About Us</a> |
             <a href = "#Calendar">Calendar</a> |
             <a href = "#Contact">Contact</a>
         </nav>
-      </center>
-       
-        
-      </header>
+
+        </center>   
     </div>
   );
 }
