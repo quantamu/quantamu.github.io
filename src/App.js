8dd3b06f5df76f60e7a0c708265207396937c940
Samuel jose Molero gonzalez <samuel@Mac-5072.lan>
2025-01-02 18:27:22 -0600

    New changes-Header

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
         <img src={'${process.env.PUBLIC_URL}/logo.png'} className="header-logo"></img>
         <nav>
           <ul>
-            <li><a href='#home'> Home </a></li>
-            <li><a href='#about-us'> About Us</a></li>
-            <li><a href ="#calendar"> Calendar</a></li>
+            <li><a href='#home'> Home </a> <a href='#about-us'> About Us</a> <a href ="#calendar"> Calendar</a></li>
           </ul>
         </nav>
         
