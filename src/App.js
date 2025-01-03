797aa228efc96e0d745aad35d0031b348f0245cc
Samuel jose Molero gonzalez <samuel@Mac-5072.lan>
2025-01-02 18:04:34 -0600

    New changes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Website-header">
+        <nav>
+          <ul>
+            <li><a href='#home'> Home </a></li>
+            <li><a href='#about-us'> About Us</a></li>
+            <li><a href ="#calendar"> Calendar</a></li>
+          </ul>
+        </nav>
+        
       </header>
     </div>
   );
