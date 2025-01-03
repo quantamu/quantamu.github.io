588062a79016909fa2ba090b58ec37d684f4c74b
Samuel jose Molero gonzalez <samuel@Mac-5162.lan>
2025-01-02 20:28:07 -0600

    Update to home

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 
-import './index.css';
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
       <header className="Website-header">
-        <img src={'${process.env.PUBLIC_URL}/logo.png'} className="header-logo"></img>
-        <nav style = "margin-top: 10px">
-    
-            <a href = "#About Us"> About Us </a> |
-            <a href = "#Calendar"> Calendar </a> |
-            <a href = "#Contact"> Contact</a>
-          
+        <img src={'src/assets/logo.png'} className="header-logo"></img>
+
+        <nav>
+            <a href = "#About Us">About Us </a> |
+            <a href = "#Calendar">Calendar </a> |
+            <a href = "#Contact">Contact</a>
         </nav>
         
       </header>
