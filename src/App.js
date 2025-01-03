cc0766b96a4fc6deb67f6f67e022fa2751d0abf0
Samuel jose Molero gonzalez <samuel@Mac-5162.lan>
2025-01-02 22:25:00 -0600

    Updates to home page

--- src/App.js
+++ src/App.js
@@ -17,19 +17,20 @@ function App() {
             <nav>
               <a href = "App.js">About Us</a> |
               <a href = "App.js">Calendar</a> |
-              <a href = "App.js">Social</a>
+              <a href = "App.js">Officers</a> |
+              <a href = "App.js">Contact</a>
             </nav>
           </div>
           
         </div>
-        <button className = "join-us-button">Join Us</button>
+        <a href = "https://discord.gg/52RwejHd" className = "join-us-button">Join Us</a>
       </header>
 
       <footer className ="Website-footer">
         <p>Contact us:</p>
         <a><img src ={mail}/></a>
-        <a><img src ={linkedin}/></a>
-        <a><img src ={insta}/></a>
+        <a href ="https://www.linkedin.com/company/qtam"><img src ={linkedin}/></a>
+        <a href = "https://www.instagram.com/tamuquantum/"><img src ={insta}/></a>
 
       </footer>
       
