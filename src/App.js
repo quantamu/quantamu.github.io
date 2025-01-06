0550521506285657c36656f922d093c639002c42
Samuel jose Molero gonzalez <samuel@Mac-6417.lan>
2025-01-05 23:53:34 -0600

    Update to homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React, {useState} from 'react';
 import logo from './assets/logo2.png'
 import insta from './assets/insta.jpg'
 import linkedin from './assets/linkedin.png'
 import mail from './assets/mail.jpg'
 import './App.css';
+import { useEffect } from 'react';
+
+const messages = [
+  "Quantum computing is an emergent field of cutting-edge computer science harnessing the unique qualities of quantum mechanics to solve problems beyond the ability of even the most powerful classical computers - IBM",
+  "Quantum computers use qubits, which can represent both 0 and 1 simultaneously, allowing for more complex computations. - Quoate from someone Smart",
+  "Quantum entanglement allows particles to be interconnected in such a way that the state of one particle can instantly influence the state of another, no matter the distance between them. - YE"
+];
 
 function App() {
+  const[currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="Website-header">
@@ -29,6 +47,10 @@ function App() {
       <main className = "home-content">
         <section>
           <h1>What is quantum Computing?</h1>
+          <div className = "transparent-box">
+            <p> <i>{messages[currentMessageIndex]}</i></p>
+          </div>
+          
         </section>
 
       </main>
