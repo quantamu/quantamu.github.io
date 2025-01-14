27cd73ba11a47c374e54220b54fd46c0562f7067
Samuel jose Molero gonzalez <samuel@Samuels-MacBook-Pro-3.local>
2025-01-14 15:02:02 -0600

    About Us page updates

--- src/About.js
+++ src/About.js
@@ -5,18 +5,26 @@ import './App.css'
 
 function About() {
     return (
-        <div className='about-container'>
-            <h1 className='title'>About us</h1>
-            <p>Howdy, and welcome to QuantA&M, Texas A&M's first student organization for those interested in learning more about quantum computing.
-                 We hope to achieve our mission by facilitating events and networking opportunities for our members, giving everyone an opportunity to 
-                 consider the vast field of quantum technology as an undergraduate. We are an inclusive org, targeting undergraduate students from all majors. 
-                 Graduate students may also participate in club activities, 
-                 but with the added option to serve as a "graduate mentor," offering advice and research assistance to undergrads on a flexible schedule. QuantA&M was
-                  founded halfway through the 2022-2023 academic year
-                , and as such, we are slowly transitioning from a small student organization to a large one. We are currently offering free membership on an unofficial basis, all of our meetings are open to the student body, with no dues required. To learn more about what we do, check out the "Events" tab, and if you have further questions, contact someone from our team. Thanks and Gig'em.
-            </p>
+        <main className='about-container'>
+            <h1>About us</h1>
+            <div className='text'>
+                <p>Howdy, and welcome to QuantA&M, Texas A&M's first student organization for those interested in learning more about quantum computing.
+                    We hope to achieve our mission by facilitating events and networking opportunities for our members, giving everyone an opportunity to 
+                    consider the vast field of quantum technology as an undergraduate. We are an inclusive org, targeting undergraduate students from all majors. 
+                    Graduate students may also participate in club activities, 
+                    but with the added option to serve as a "graduate mentor," offering advice and research assistance to undergrads on a flexible schedule. QuantA&M was
+                    founded halfway through the 2022-2023 academic year
+                    , and as such, we are slowly transitioning from a small student organization to a large one. We are currently offering free membership on an unofficial basis, all of our meetings are open to the student body, with no dues required. To learn more about what we do, check out the "Events" tab, and if you have further questions, contact someone from our team.
+                </p> 
+            </div>
+            <p className = 'text2'> <b> Thanks and Gig'em! </b> </p>
 
-        </div>
+
+            <div className = "Additional">
+            </div>
+           
+
+        </main>
     );
 }
 export default About;
