a9ab312fcbf210b6c5f0007f883d5a95e9295955
Samuel jose Molero gonzalez <samuel@Samuels-MacBook-Pro-5.local>
2025-01-18 21:51:57 -0600

    Udpates to officer

--- src/Officers.js
+++ src/Officers.js
@@ -2,22 +2,41 @@ import React from 'react';
 import './Officers.css'
 import sam from './assets/officers/sam.png'
 import arjum from './assets/officers/Arjum.png'
+import samuel from './assets/officers/samuel.png'
+import andrii from  './assets/officers/andrii.png'
+import huang from './assets/officers/huang.png'
 
 function officers()  {
 
     return (
         <main> 
             <h1 className ='header-section'>Our Team</h1>
-            <div className = 'team-content'>
-                <div className = 'box'>
-                    <img src = {sam} className = 'officer-images'/>
+                <div className = 'team-content'>
+                    <div className = 'box'>
+                        <img src = {sam} className = 'officer-images'/>
+                        <p> Samuel Bieberich, President</p>
+                    </div>
+                    <div className = 'box'>
+                        <img src = {arjum} className = 'officer-images'/>
+                        <p> Arjun Som, Vice President</p>
+                    </div>
+                    <div className = 'box'>
+                        <img src = {samuel} className = 'officer-images'/>
+                        <p> Samuel Molero, Secretary & Webmaster</p>
+                    </div>
                 </div>
-                <p> Samuel Bieberich, President</p>
-                <div className = 'box'>
-                    <img src = {arjum} className = 'officer-images'/>
+                <div className = 'team-content'>
+                    <div className = 'box'>
+                        <img src = {huang} className = 'officer-images'/>
+                        <p> Wie-Chih Huang, Graduate Mentor</p>
+                    </div>
+                    <div className = 'box'>
+                        <img src = {andrii} className = 'officer-images'/>
+                        <p> Andrii Kryvenko, Treasurer </p>
+                    </div>
+
                 </div>
-                <p> Arjun Som, Vice President</p>
-            </div>
+
 
 
             
