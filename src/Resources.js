d8e186ad0d7221e63593823e0709a6b17e34c69c
Sam-Bieberich <sambieberich@gmail.com>
2025-01-21 13:31:50 -0600

    link integration

--- src/Resources.js
+++ src/Resources.js
@@ -2,29 +2,40 @@ import React, { useEffect, useState } from 'react';
 import './Resources.css';
 
 function Resources() {
-  const [resources, setResources] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     fetch('/resources.json')
       .then(response => response.json())
-      .then(data => setResources(data))
+      .then(data => setCategories(data))
       .catch(error => console.error('Error fetching resources:', error));
   }, []);
 
   return (
     <main className='resources-container'>
       <h1 className='header-section'>Resources</h1>
-      <div className='resource-list'>
-        <ul>
-          {resources.map((resource, index) => (
-            <li key={index}>
-              <a href={`/resources/${resource.file}`} target="_blank" rel="noopener noreferrer">
-                {resource.title} ({resource.date})
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {categories.map((category, index) => (
+        <div key={index} className='resource-category'>
+          <h2 className='category-title'>{category.category}</h2>
+          <div className='resource-list'>
+            <ul>
+              {category.resources.map((resource, index) => (
+                <li key={index}>
+                  {resource.type === 'pdf' ? (
+                    <a href={`/resources/${resource.file}`} target="_blank" rel="noopener noreferrer">
+                      {resource.title} ({resource.date})
+                    </a>
+                  ) : (
+                    <a href={resource.url} target="_blank" rel="noopener noreferrer">
+                      {resource.title} ({resource.date})
+                    </a>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
     </main>
   );
 }
