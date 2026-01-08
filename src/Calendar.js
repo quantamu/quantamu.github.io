bb04e3214b0f86738dc03cb135b5c01f7f90451f
Sam B <107863539+Sam-Bieberich@users.noreply.github.com>
2026-01-08 10:40:38 -0600

    Adjust margin for calendar iframe
    Increase top margin of calendar display.

--- src/Calendar.js
+++ src/Calendar.js
@@ -5,7 +5,7 @@ function Calendar() {
     return (
         <main style={{ minHeight: 'calc(100vh - 300px)' }}>
             <h1 className ='header-section'><strong>Calendar</strong></h1>
-            <div style = {{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', marginTop: '20px'}}>
+            <div style = {{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', marginTop: '50px'}}>
                 <iframe 
                 src = "https://calendar.google.com/calendar/embed?src=tamu.quantum%40gmail.com&ctz=America%2FChicago"
                 style = {{border: 0, marginBottom:'-50px'}}
